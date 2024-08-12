a3ab257f393c08e385011a5cba716160fcd55027
KimAeris <kar2125@naver.com>
2024-08-12 12:06:12 +0900

    lint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,18 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
+  extends: [
+    'plugin:vue/vue3-essential', //  Vue 3 필수 ESLint 규칙
+    'eslint:recommended', // 기본 ESLint 권장 규칙
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': ['error', { allow: ['info', 'warn', 'error'] }]
   }
-}
+};
