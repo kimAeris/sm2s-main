a75b3ec7eb21a7136089c68f6e279745555dee73
KimAeris <kar2125@naver.com>
2024-08-19 10:32:59 +0900

    메인 레이아웃 변경 (로그인 분리)

--- src/layouts/TheView.vue
+++ src/layouts/TheView.vue
@@ -1,6 +1,10 @@
 <template>
   <VMain>
-    <VContainer class="px-4 py-2 body-height" fluid>
+    <VContainer v-if="!header" class="h-100" fluid>
+      <RouterView />
+    </VContainer>
+
+    <VContainer v-else class="px-4 py-2 body-height" fluid>
       <div class="d-flex flex-column w-100 h-100 overflow-hidden">
         <RouterView />
       </div>
@@ -9,5 +13,8 @@
 </template>
 
 <script setup>
+import { useLayout } from '@/stores/useLayout';
 import { RouterView } from 'vue-router';
+
+const { header } = useLayout();
 </script>
