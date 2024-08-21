6b45c31493cb9e26008b5263532fc0afa9604cb8
KimAeris <kar2125@naver.com>
2024-08-21 09:47:44 +0900

    user api 등록

--- src/api/user.js
+++ src/api/user.js
@@ -1,12 +1,51 @@
 import { adminUser } from '.';
 
+export const getUserList = async (params) => {
+  try {
+    const res = await adminUser.get('', params);
+
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const saveUser = async (params) => {
+  try {
+    const res = await adminUser.get('/status/1', params);
+
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteUser = async (params) => {
+  try {
+    const res = await adminUser.get('/status/3', params);
+
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const passwordChange = async (params) => {
   try {
     const res = await adminUser.post(`/password/status/1`, params);
 
     return res.data;
   } catch (error) {
-    console.error('Error fetching user:', error);
+    throw error;
+  }
+};
+
+export const initPassword = async (params) => {
+  try {
+    const res = await adminUser.post(`/init/status/1`, params);
+
+    return res.data;
+  } catch (error) {
     throw error;
   }
 };
