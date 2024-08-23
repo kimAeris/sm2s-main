e2cb1fec89cdc0eb484fef157e01b405bcdde93f
KimAeris <kar2125@naver.com>
2024-08-23 18:01:45 +0900

    메뉴관리

--- src/api/system/menus.js
+++ src/api/system/menus.js
@@ -4,7 +4,9 @@ export const getMenus = async (params) => {
   try {
     const res = await menus.get('', { params });
 
-    return res.data;
+    if (res.data.header.code !== 200) throw res;
+
+    return res.data.body.list;
   } catch (error) {
     throw error;
   }
--- src/views/system/MenuMgmtView.vue
+++ src/views/system/MenuMgmtView.vue
@@ -1,47 +1,65 @@
 <template>
   <ContentHeader v-model:filters="searchFilters" />
 
-  <VRow>
-    <VCol cols="3" class="pr-0">
-      <ContentBody class="h-100" title="메뉴">
-        <VList v-model:opened="open">
-          <VListGroup
-            v-for="menus in menuList"
-            :key="menus.projectCd"
-            :value="menus.projectCd"
-          >
-            <template v-slot:activator="{ props }">
-              <VListItem v-bind="props" :title="menus.projectNm"></VListItem>
-            </template>
-
+  <div class="d-flex overflow-hidden h-100 ga-4" style="padding-bottom: 1px">
+    <ContentBody title="메뉴" min-width="300">
+      <VList v-model="list">
+        <VListItem title="전체 목록" value="all" @click="handleAll">
+        </VListItem>
+        <VListGroup
+          v-for="menus in menuList"
+          :key="menus.projectCode"
+          :value="menus.projectName"
+        >
+          <template v-slot:activator="{ props }">
             <VListItem
-              v-for="({ mainMenuNm, mainMenuCd }, i) in menus.menu"
-              :key="i"
-              :title="mainMenuNm"
-              :value="mainMenuCd"
+              v-bind="props"
+              :title="menus.projectName"
+              @click="handleProject(menus.projectCode)"
             ></VListItem>
-          </VListGroup>
-        </VList>
-      </ContentBody>
-    </VCol>
-    <VCol>
-      <ContentBody class="h-100" />
-    </VCol>
-  </VRow>
+          </template>
+
+          <VListItem
+            v-for="menu in menus.mainMenu"
+            :key="menu.menuCode"
+            :title="menu.menuName"
+            :value="menu.menuCode"
+            @click="handleMain(menu)"
+          ></VListItem>
+        </VListGroup>
+      </VList>
+    </ContentBody>
+
+    <ContentBody>
+      <VDataTable
+        class="h-100 overflow-auto"
+        v-model="selectedItems"
+        :headers="headers"
+        :items="items"
+        :loading="loading"
+        show-select
+      >
+      </VDataTable>
+    </ContentBody>
+  </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useUser } from '@/stores/useUser';
-import { useMenu } from '@/stores/useMenu';
+import { getMenus } from '@/api/system/menus';
 
-const { menus } = storeToRefs(useMenu());
 const { projectList } = storeToRefs(useUser());
 
+const list = ref('all');
+const allMenuList = ref([]);
 const menuList = ref([]);
 
-const open = ref(['Users']);
+const loading = ref(false);
+const selectedItems = ref([]);
+const items = ref([]);
+const addItems = ref([]);
 
 const setProjectOptions = () => {
   const list = projectList.value.map((list) => ({
@@ -73,27 +91,82 @@ const searchFilters = ref([
   }
 ]);
 
-const getMenuList = () => {
-  const result = projectList.value.map((project) => ({
-    projectCd: project.projectCode,
-    projectNm: project.projectName
-  }));
+const setMenuList = async () => {
+  try {
+    const res = await getMenus();
+    allMenuList.value = res;
 
-  result.forEach((project) => {
-    const menuList = menus.value.find(
-      (menu) => menu.projectCode === project.projectCd
-    );
+    const result = res.reduce((acc, obj) => {
+      let project = acc.find((p) => p.projectCode === obj.projectCode);
 
-    project.menu = menuList?.mainMenu.map((menu) => ({
-      mainMenuCd: menu.mainMenuCd,
-      mainMenuNm: menu.mainMenuNm
-    }));
-  });
+      if (!project) {
+        project = {
+          projectCode: obj.projectCode,
+          projectName: obj.projectName,
+          mainMenu: []
+        };
+        acc.push(project);
+      }
 
-  menuList.value = result;
+      if (obj.level === '1') {
+        project.mainMenu.push({
+          ...obj,
+          subMenu: []
+        });
+      } else if (obj.level === '2') {
+        const mainMenu = project.mainMenu.find(
+          (item) => item.menuCode === obj.parentMenuCode
+        );
+
+        if (mainMenu) {
+          mainMenu.subMenu.push(obj);
+        }
+      }
+
+      return acc;
+    }, []);
+
+    menuList.value = result;
+
+    console.log('result', result);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
+const handleAll = () => {
+  items.value = allMenuList.value;
+};
+
+const handleProject = (selected) => {
+  items.value = menuList.value.find(
+    (menu) => menu.projectCode === selected
+  ).mainMenu;
+};
+
+const handleMain = (selected) => {
+  const mainMenu = menuList.value.find(
+    (item) => item.projectCode === selected.projectCode
+  ).mainMenu;
+
+  items.value = mainMenu.find(
+    (item) => item.menuCode === selected.menuCode
+  ).subMenu;
+};
+
+const headers = [
+  { title: '메뉴 코드', key: 'menuCode' },
+  { title: '메뉴명', key: 'menuName' },
+  { title: '메뉴 설명', key: 'menuDesc' },
+  { title: '사용여부', key: 'useYn' },
+  { title: '정렬번호', key: 'sortNo' },
+  { title: '등록자', key: 'regNm' },
+  { title: '등록일', key: 'regDt' },
+  { title: '수정자', key: 'chgNm' },
+  { title: '수정일', key: 'chgDt' }
+];
+
 onMounted(() => {
-  getMenuList();
+  setMenuList();
 });
 </script>
