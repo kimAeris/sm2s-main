c199db3633b2294b55489e1b54a4e1a4a1f00907
KimAeris <kar2125@naver.com>
2024-08-26 10:26:23 +0900

    vite 개발 서버 환경설정

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,46 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vuetify()],
+export default defineConfig(({ mode }) => ({
+  plugins: [vue(), vuetify(), envInfoPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 5173,
+    open: true,
+    proxy: {
+      '/api': {
+        target: loadEnv(mode, process.cwd()).VITE_BASE_URL,
+        changeOrigin: true,
+        protocolRewrite: true
+        // rewrite: (path) => {
+        //   if (mode === 'localdev') return path.replace(/^\/api/, '');
+        //   return path;
+        // }
+      }
+    }
+  }
+}));
+
+const envInfoPlugin = () => ({
+  name: 'env-info',
+  configResolved: ({ mode, env }) => {
+    console.info('\x1b[32m%s\x1b[0m', '\n  Running Env Status 🚀\n');
+    console.info(
+      '\x1b[34m%s\x1b[0m',
+      '  ✅ Mode:   ',
+      `${mode === 'localdev' ? 'Local' : mode.replace(/^\w/, (c) => c.toUpperCase())}`
+    );
+    console.info(
+      '\x1b[35m%s\x1b[0m',
+      '  🌐 Server: ',
+      `${env.VITE_BASE_URL}\n`
+    );
   }
 });
