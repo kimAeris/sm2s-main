8c788f91b9afab2bc2fae24681aee86ad1c69d8a
KimAeris <kar2125@naver.com>
2024-08-27 16:46:47 +0900

    로그아웃 api 추가

--- src/api/common.js
+++ src/api/common.js
@@ -10,6 +10,14 @@ export const login = async (businessNumber, userId, userPwd) => {
 
     return res.data;
   } catch (error) {
+    throw error;
+  }
+};
+
+export const logout = async () => {
+  try {
+    await common.post(`/logout`, {});
+  } catch (error) {
     console.error('Error fetching user:', error);
     throw error;
   }
