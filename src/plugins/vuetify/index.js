4c814468cc61c111493dd3198ee007a9e64194cf
KimAeris <kar2125@naver.com>
2024-08-28 12:46:57 +0900

    DayJsAdapter 설정

--- src/plugins/vuetify/index.js
+++ src/plugins/vuetify/index.js
@@ -1,14 +1,29 @@
 import '@mdi/font/css/materialdesignicons.css';
 import '@/styles/main.scss';
 
+import { ko } from 'vuetify/locale';
+import 'dayjs/locale/ko';
+import DayJsAdapter from '@date-io/dayjs';
+
 import theme from '@/plugins/vuetify/config/theme';
 import defaults from '@/plugins/vuetify/config/defaults';
 import { createVuetify } from 'vuetify';
-import { ko } from 'vuetify/locale';
 
 export default createVuetify({
   theme,
   defaults,
+  date: {
+    // locale: {
+    //   en: 'ko'
+    // },
+    adapter: DayJsAdapter,
+    formats: {
+      isoFullDate: 'YYYY-MM-DD',
+      isoYearAndMonth: 'YYYY-MM',
+      isoFullDateTime: 'YYYY-MM-DD HH:MM',
+      isoDateWithWeekday: 'MM-DD (ddd)'
+    }
+  },
   locale: {
     locale: 'ko',
     messages: { ko }
