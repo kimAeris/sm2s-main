e0a341cb54faec0f1073964262465f8cdbed5b6d
KimAeris <kar2125@naver.com>
2024-09-04 09:03:24 +0900

    로그아웃 핸들러 추가

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,27 @@ import TheSideBar from '@/layouts/TheSideBar.vue';
 import TheView from '@/layouts/TheView.vue';
 import AppToast from './components/app/AppToast.vue';
 import AppFeedback from './components/app/AppFeedback.vue';
-</script>
+import { onUnmounted } from 'vue';
+import { logout } from '@/api/common';
+import { useUser } from './stores/useUser';
+import { useMenu } from './stores/useMenu';
 
-<style scoped></style>
+const userStore = useUser();
+const menuStore = useMenu();
+
+// TODO: 로직 수정 필요 (작동X)
+const handleLogout = async () => {
+  try {
+    await logout();
+
+    userStore.resetUser();
+    menuStore.resetMenu();
+  } catch (error) {
+    if (import.meta.env.DEV) console.error(error);
+  }
+};
+
+onUnmounted(() => {
+  handleLogout();
+});
+</script>
